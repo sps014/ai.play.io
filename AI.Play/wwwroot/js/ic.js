4a4fa8c3ef3907e0f5557c7e72260cc896461024
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-09-01 22:45:10 +0530

    testing ic with mobilenet

---
+++ AI.Play/wwwroot/js/ic.js
@@ -0,0 +1,23 @@
+﻿let featureExtractor;
+
+function CreateFeatureExtractor(Dotnet)
+{
+    featureExtractor = ml5.featureExtractor('MobileNet', ModelLoaded.bind(Dotnet));
+}
+
+function ModelLoaded() {
+    this.invokeMethodAsync("ICModelLoad");
+}
+function AddImage(imageElementRef, label) {
+    console.log(imageElementRef);
+    featureExtractor.addImage(imageElementRef , label);
+}
+
+function Train(Dotnet,report) {
+    featureExtractor.train((lossValue) =>
+    {
+        console.log(lossValue + "  ->>js");
+        if (report)
+            Dotnet.invokeMethodAsync("ICTrain", lossValue);
+    });
+}

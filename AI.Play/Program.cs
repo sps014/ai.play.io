3a513840b7d8674c9dbec0cfec42d350bdeb7548
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-09-01 20:38:48 +0530

    ..

--- AI.Play/Program.cs
+++ AI.Play/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace AI.Load
+namespace AI.Play
 {
     public class Program
     {

650b95f528ecfd34f6fa1fbbd8539cc4acaae65b
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-09-08 17:49:37 +0530

    added image reference

--- AI.Play/Models/ImageClassContainer.cs
+++ AI.Play/Models/ImageClassContainer.cs
@@ -1,7 +1,13 @@
+using Microsoft.AspNetCore.Components;
 using System;
 
 public class ImageClassContainer
 {
     public string ClassName { get; set; }
-    public string[] Images { get; set; } = Array.Empty<string>();
+    public ImageRefs[] Images { get; set; } = Array.Empty<ImageRefs>();
+}
+public class ImageRefs
+{
+    public string Image { get; set; }
+    public ElementReference Reference { get; set; }
 }

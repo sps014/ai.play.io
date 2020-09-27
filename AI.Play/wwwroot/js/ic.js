925ce9d262884defc512db80cd25d97ca814e038
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-09-27 15:37:54 +0530

    train image add fixed

--- AI.Play/wwwroot/js/ic.js
+++ AI.Play/wwwroot/js/ic.js
@@ -1,7 +1,6 @@
 ﻿let featureExtractor;
 
-function CreateFeatureExtractor(Dotnet)
-{
+function CreateFeatureExtractor(Dotnet) {
     featureExtractor = ml5.featureExtractor('MobileNet', ModelLoaded.bind(Dotnet));
     featureExtractor = featureExtractor.classification();
 }
@@ -10,13 +9,11 @@ function ModelLoaded() {
     this.invokeMethodAsync("ICModelLoad");
 }
 function AddImage(imageElementRef, label) {
-    console.log(imageElementRef);
     featureExtractor.addImage(imageElementRef, label);
 }
 
-function Train(Dotnet,report) {
-    featureExtractor.train((lossValue) =>
-    {
+function Train(Dotnet, report) {
+    featureExtractor.train((lossValue) => {
         if (report)
             Dotnet.invokeMethodAsync("ICTrain", lossValue);
     });

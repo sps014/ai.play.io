889f3f5353d000d215933dea15fa535a0b7e6585
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-10-06 16:27:47 +0530

    refactoring c#

---
+++ AI.Play/Components/ClassModifyView.razor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+
+namespace AI.Play.Components
+{
+    public partial class ClassModifyView
+    {
+        int ColCount = 4;
+        [Parameter]
+        public string Index { get; set; }
+        int i => int.Parse(Index);
+        ImageClassContainer Class => ImageClassView.Classes[i];
+
+        void RemoveImage(ImageRefs refs)
+        {
+            if (Class.Images.Contains(refs))
+                Class.Images.Remove(refs);
+            StateHasChanged();
+        }
+        async Task OnInputFileChange(InputFileChangeEventArgs e)
+        {
+            var imageFiles = e.GetMultipleFiles(1024);
+
+            var format = "image/png";
+            foreach (var imageFile in imageFiles)
+            {
+                var resizedImageFile = await imageFile.RequestImageFileAsync(format, 400, 400);
+                var buffer = new byte[resizedImageFile.Size];
+                await resizedImageFile.OpenReadStream().ReadAsync(buffer);
+                var imageDataUrl = $"data:{format};base64,{Convert.ToBase64String(buffer)}";
+                Class.Images.Add(new ImageRefs() { Image = imageDataUrl });
+            }
+
+            StateHasChanged();
+        }
+
+    }
+}
---
+++ AI.Play/Components/ImageTrainView.razor.cs
@@ -0,0 +1,106 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace AI.Play.Components
+{
+    public partial class ImageTrainView
+    {
+        bool IsModelLoaded = false;
+        static int imageAddedCount = 0;
+        bool IsImageAddedBegin = false;
+        bool trainingBegin = false;
+        static double loss = 0;
+        public static bool OnceTrained = false;
+        DotNetObjectReference<ImageTrainView> reference;
+        bool waitforLoad = false;
+
+        protected override void OnInitialized()
+        {
+            reference = DotNetObjectReference.Create(this);
+            OnLoad();
+        }
+        async void OnLoad()
+        {
+            await Runtime.InvokeVoidAsync("CreateFeatureExtractor", reference);
+        }
+        async Task AddImage(ElementReference image, string label)
+        {
+            await Runtime.InvokeVoidAsync("AddImage", image, label);
+        }
+        async Task Train(bool report = true)
+        {
+            if (!waitforLoad)
+            {
+                await ReloadModel();
+                waitforLoad = true;
+                return;
+            }
+            OnceTrained = false;
+
+            //Add images to NN
+            IsImageAddedBegin = true;
+            StateHasChanged();
+            await Task.Delay(1);
+            imageAddedCount = 0;
+            foreach (var cls in ImageClassView.Classes)
+                foreach (var img in cls.Images)
+                {
+                    imageAddedCount++;
+                    await AddImage(img.Reference, cls.ClassName);
+                    StateHasChanged();
+                    await Task.Delay(1);
+                }
+            IsImageAddedBegin = false;
+            trainingBegin = true;
+            StateHasChanged();
+            await Task.Delay(1);
+            await Runtime.InvokeVoidAsync("Train", reference, report);
+        }
+
+        async Task ReloadModel()
+        {
+            await Runtime.InvokeVoidAsync("ReloadModel", reference, ImageClassView.Classes.Count);
+        }
+
+        [JSInvokable("ICModelLoad")]
+        public async void ModelLoaded()
+        {
+            IsModelLoaded = true;
+
+            if (waitforLoad)
+            {
+                await Train();
+                waitforLoad = false;
+            }
+
+            StateHasChanged();
+        }
+        [JSInvokable("ICTrain")]
+        public async void OnTrain(string loss)
+        {
+            if (string.IsNullOrWhiteSpace(loss))
+            {
+                trainingBegin = false;
+                OnceTrained = true;
+                TrainingDone?.Invoke();
+            }
+            else
+            {
+                ImageTrainView.loss = double.Parse(loss);
+            }
+            //Console.WriteLine(loss);
+            StateHasChanged();
+            await Task.Delay(1);
+        }
+        public void Refresh()
+        {
+            StateHasChanged();
+        }
+
+        public delegate void TrainingDoneHandler();
+        public event TrainingDoneHandler TrainingDone;
+        int imagesCount => ImageClassView.Classes.Select(x => x.Images.Count).Sum();
+    }
+}
